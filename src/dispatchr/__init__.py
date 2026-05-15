36bb595a1ccbf2846147e7ea8f750015117f796d
Kevin Rieck <coalbite@protonmail.com>
2026-05-15 08:15:29 +0200

    fix: export bus draining error

--- src/dispatchr/__init__.py
+++ src/dispatchr/__init__.py
@@ -2,6 +2,7 @@
 
 from dispatchr.bus import MessageBus
 from dispatchr.exceptions import (
+    BusDrainingError,
     DispatchrError,
     DuplicateCommandHandlerError,
     EventPublicationError,
@@ -19,6 +20,7 @@ from dispatchr.observability import (
 __all__ = [
     "DispatchFinished",
     "DispatchStarted",
+    "BusDrainingError",
     "DispatchrError",
     "DuplicateCommandHandlerError",
     "EventPublicationError",
--- tests/test_message_bus.py
+++ tests/test_message_bus.py
@@ -1006,6 +1006,6 @@ async def test_aclose_stops_background_loop_created_by_sync_bridge() -> None:
 
 
 def test_public_api_exports_bus_draining_error() -> None:
-    from dispatchr.exceptions import BusDrainingError
+    from dispatchr import BusDrainingError, DispatchrError
 
-    assert issubclass(BusDrainingError, Exception)
+    assert issubclass(BusDrainingError, DispatchrError)
